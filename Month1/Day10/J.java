09cea1a3bc0eefe545ccc8bd0cccd4c959c434fc
as7926946-alt <as7926946@gmail.com>
2026-09-07 13:38:18 +0530

    Java

--- Month1/Day10/J.java
+++ Month1/Day10/J.java
@@ -62,25 +62,25 @@
 //     }
 // }
 
-public class J{
-    public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
-        int row=sc.nextInt();
-        int column=sc.nextInt();
-        int arr[][]=new int[row][column];
-        int max=arr[0][0];
-        for(int i=0;i<row;i++){
-            for(int j=0;j<column;j++){
-                arr[i][j]=sc.nextInt();
-            }
-        }
-        for(int j=0;j<column;j++){
-            for(int i=0;i<row;i++){
-              if(max<arr[i][j]){
-                max=arr[i][j];
-              }
-            }
-            System.out.println(max);
-        }
-    }
-}
+// public class J{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         int row=sc.nextInt();
+//         int column=sc.nextInt();
+//         int arr[][]=new int[row][column];
+//         for(int i=0;i<row;i++){
+//             for(int j=0;j<column;j++){
+//                 arr[i][j]=sc.nextInt();
+//             }
+//         }
+//           int max=arr[0][0];
+//         for(int j=0;j<column;j++){
+//             for(int i=0;i<row;i++){
+//               if(max<arr[i][j]){
+//                 max=arr[i][j];
+//               }
+//             }
+//         }
+//         System.out.println(max);
+//     }
+//}
