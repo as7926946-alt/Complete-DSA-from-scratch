b9cd7e761cdbdf6cde5ce78368fae8766df62096
as7926946-alt <as7926946@gmail.com>
2026-09-07 13:34:26 +0530

    Java

---
+++ Month1/Day10/J.java
@@ -0,0 +1,86 @@
+ import java.util.Scanner;
+// public class J {
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         int row=sc.nextInt();
+//         int column=sc.nextInt();
+//         int arr[][]=new int[row][column];
+//         for(int i=0;i<row;i++){
+//             for(int j=0;j<column;j++){
+//                 arr[i][j]=sc.nextInt();
+//             }
+//         }
+//          for(int i=0;i<row;i++){
+//             for(int j=0;j<column;j++){
+//                 System.out.print(arr[i][j] + " ");
+//             }
+//             System.out.println();
+//     }
+    
+// }
+// }
+
+// public class J{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         int row=sc.nextInt();
+//         int column=sc.nextInt();
+//         int arr[][]=new int [row][column];
+//         for(int i=0;i<row;i++){
+//             for(int j=0;j<column;j++){
+//                 arr[i][j]=sc.nextInt();
+//             }
+//         }
+//         for(int i=0;i<row;i++){
+//              int sum=0;
+//             for(int j=0;j<column;j++){
+//                 sum=sum+arr[i][j];
+//             }
+//             System.out.println(sum);
+//         }
+//     }
+// }
+
+// public class J{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         int row=sc.nextInt();
+//         int column=sc.nextInt();
+//         int arr[][]=new int[row][column];
+//         for(int i=0;i<row;i++){
+//             for(int j=0;j<column;j++){
+//                 arr[i][j]=sc.nextInt();
+//             }
+//         }
+//         for(int j=0;j<column;j++){
+//             int sum=0;
+//             for(int i=0;i<row;i++){
+//                 sum=sum+arr[i][j];
+//             }
+//             System.out.println(sum);
+//         }
+//     }
+// }
+
+public class J{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int row=sc.nextInt();
+        int column=sc.nextInt();
+        int arr[][]=new int[row][column];
+        int max=arr[0][0];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int j=0;j<column;j++){
+            for(int i=0;i<row;i++){
+              if(max<arr[i][j]){
+                max=arr[i][j];
+              }
+            }
+            System.out.println(max);
+        }
+    }
+}
--- Month1/Day9/J.java
+++ Month1/Day9/J.java
@@ -19,3 +19,100 @@ import java.util.*;
 //     }
 // }
 
+// public class J{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner (System.in);
+//         int even=0;
+//         int odd=0;
+//         int size=sc.nextInt();
+//         int arr[]=new int[size];
+//         for(int i=0;i<size;i++){
+//             arr[i]=sc.nextInt();
+//         }
+//         for(int i=0;i<size;i++){
+//             if(arr[i]%2==0){
+//                 even++;
+//             }
+//             else{
+//                 odd++;
+//             }
+//         }
+//         System.out.println("Even = " + even);
+//         System.out.println("Odd = " + odd);
+//     }
+// }
+
+// public class J{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         int size=sc.nextInt();
+//         int arr[]=new int[size];
+//         for(int i=0;i<size;i++){
+//             arr[i]=sc.nextInt();
+//         }
+//          int max=arr[0];
+//          int sl=arr[0];
+//         for(int i=0;i<size;i++){
+//             if(max<arr[i]){
+//                 sl=max;
+//                 max=arr[i];
+//             }
+//             else if(arr[i]>sl){
+//                 sl=arr[i];
+//             }
+
+//         }
+//         System.out.println(sl);
+//     }
+// }
+
+// public class J{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         int size=sc.nextInt();
+//         int arr[]=new int[size];
+//         for(int i=0;i<size;i++){
+//             arr[i]=sc.nextInt();
+//         }
+//         int pos=0;
+//         int neg=0;
+//         int zero=0;
+//         for(int i=0;i<size;i++){
+//             if(arr[i]>0){
+//                 pos++;
+//             }
+//             else if(arr[i]<0){
+//                 neg++;
+//             }
+//             else{
+//                 zero++;
+//             }
+//         }
+//         System.out.println("Positive = " + pos);
+//         System.out.println("Negative = "+ neg);
+//         System.out.println("Zero = " + zero);
+//     }
+// }
+
+public class J{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        boolean sorted=true;
+        for(int i=0;i<size-1;i++){
+            if(arr[i]>arr[i+1]){
+            sorted=false;
+            }
+        }
+        if(sorted){
+            System.out.println("Sorted");
+        }
+        else{
+            System.out.println("Not Sorted");
+        }
+    }
+}
