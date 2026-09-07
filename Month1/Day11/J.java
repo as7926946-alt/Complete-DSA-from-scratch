4ab619b1a97d4444db2d015881035393c6b47554
as7926946-alt <as7926946@gmail.com>
2026-09-07 15:33:34 +0530

    String

---
+++ Month1/Day11/J.java
@@ -0,0 +1,62 @@
+import java.util.*;
+// public class J {
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         String s=sc.nextLine();
+//         System.out.println(s.length());
+//     }
+    
+// }
+
+// public class J{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         String s=sc.nextLine();
+//         for(int i=0;i<s.length();i++){
+//             System.out.println(s.charAt(i));
+
+//         }
+//     }
+// }
+
+// public class J{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         String s=sc.nextLine();
+//         for(int i=s.length()-1;i>=0;i--){
+//             System.out.println(s.charAt(i));
+//         }
+//     }
+// }
+
+// public class J{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         String s=sc.nextLine();
+//         s=s.toLowerCase();
+//          int count=0;
+//         for(int i=0;i<s.length();i++){
+//             char ch=s.charAt(i);
+//             if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+//                 count++;
+//             }
+//         }
+//         System.out.println(count);
+//     }
+// }
+
+// public class J{
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         String s=sc.nextLine();
+//         int count=0;
+//         char ch=sc.next().charAt(0);
+//         for(int i=0;i<s.length();i++){
+//             if(s.charAt(i)==ch){
+//                 count++;
+//             }
+//         }
+//         System.out.println(count);
+        
+//     }
+// }
