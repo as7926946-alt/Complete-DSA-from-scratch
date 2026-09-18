1bd64e57b0bf0f8ba453f3a8626df4bf839b2133
as7926946-alt <as7926946@gmail.com>
2026-09-18 14:23:00 +0530

    Java Collections

---
+++ Month1/Day19/J.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+// public class J {
+//     public static void main(String[]args){
+//         ArrayList<Integer> list=new ArrayList<>();
+//         list.add(10);
+//         list.add(20);
+//         list.add(30);
+//         list.add(40);
+//         list.add(50);
+//         System.out.println(list);
+//        System.out.println(list.get(2));
+//         list.set(1,25);
+//         list.remove(3);
+//         System.out.println(list);
+//         System.out.println(list.size());
+//     }
+// }
+
+// public class J{
+//     public static void main(String[]args){
+//         LinkedList<String> list=new LinkedList<>();
+//         list.add("Apple");
+//         list.add("Mango");
+//         list.add("Banana");
+//         list.add("Orange");
+//         System.out.println(list);
+// System.out.println(list.get(2));
+// System.out.println(list.set(1, "Grapes"));
+// System.out.println(list.remove(0));
+// System.out.println(list);
+// System.out.println(list.size());
+//     }
+// }
+
+// public class J{
+//     public static void main(String[]args){
+//         Stack<Integer> stack=new Stack<>();
+//         stack.push(10);
+//         stack.push(20);
+//         stack.push(30);
+//         stack.push(40);
+//         System.out.println(stack);
+//         System.out.println(stack.peek());
+//         stack.pop();
+//         System.out.println(stack);
+//         System.out.println(stack.size());
+//     }
+// }
+
+// public class J{
+//     public static void main(String[]args){
+//         Queue<Integer> q=new LinkedList<>();
+//         q.offer(10);
+//         q.offer(20);
+//          q.offer(30);
+//          q.offer(40);
+//          System.out.println(q);
+//         System.out.println(q.peek());
+//         q.poll();
+//         System.out.println(q);
+//         System.out.println(q.size());
+
+
+//     }
+// }
+
+
